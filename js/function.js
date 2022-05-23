45322452c6f68a3c0828a46f2b4b141db422c6b1
Jose Marín <jmarinmalagon@gmail.com>
2022-05-23 17:20:25 +0200

    udpate project

---
+++ js/function.js
@@ -0,0 +1,19 @@
+let cadena = 0;
+
+function getValue(newValor){
+    let boton = document.getElementById(newValor).innerHTML;
+    cadena += boton;
+    document.getElementById('lcd').value = cadena;
+}
+
+function setDefault(){
+    document.getElementById('lcd').value = '0.00';
+    cadena = ''
+}
+
+function getResult(){
+    // let result = eval(cadena);
+    document.getElementById('lcd').value= cadena;
+}
+
+// function getResult(){ }
